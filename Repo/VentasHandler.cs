1a0be5a63e64b4894a24d5203bceb772b4b9c8b0
Ale-p650 <ale.pavlinusich@gmail.com>
2022-08-18 11:24:17 -0300

    Copia de los directorios "Models" y "Repo"
    Se copiaron los directorios del proyecto "Pre-Entrega 1"

---
+++ Repo/VentasHandler.cs
@@ -0,0 +1,39 @@
+﻿using ConsoleApp.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConsoleApp
+{
+    public class VentasHandler : DBHandler
+    {
+        public List<Venta> GetVentas()
+        {
+            List<Venta> ventas = new List<Venta>();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                string query = "SELECT * FROM VENTA";
+
+                using (SqlCommand command = new SqlCommand(query, conn))
+                {
+                    conn.Open();
+                    using (SqlDataReader dr = command.ExecuteReader())
+                    {
+                        if (dr.HasRows)
+                        {
+                            while (dr.Read())
+                            {
+                                Venta venta = new Venta();
+                                venta.Id = Convert.ToInt32(dr["Id"]);
+
+
+                                ventas.Add(venta);
+                            }
+                        }
+                    }
+                    conn.Close();
+                }
+            }
+            return ventas;
+        }
+    }
+}
